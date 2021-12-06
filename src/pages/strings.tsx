c786a8f93469dbb0ebcd8cf90d6ed6f6246997c7
vera-barysava <Vera.Barysava@vertexinc.com>
2021-12-06 20:04:57 +0100

    Update config files

---
+++ src/pages/strings.tsx
@@ -0,0 +1,6 @@
+export const strings = {
+  // Form Titles
+  CustomerExemptionFormTitle: 'fsfsfsfsNew Customer Exemptions Report catagary',
+  RegistrationsFormTitle: 'Catagary',
+  LocationsFormTitle: 'nme'
+};
---
+++ src/strings.tsx
@@ -0,0 +1,5 @@
+export const strings = {
+  // Form Titles
+  
+  NewTaxAssistLookupFormTitle: 'Wroooong isue New Tax Assist Lookup Tables Report',
+};
